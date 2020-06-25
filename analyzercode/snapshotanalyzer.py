1ee0cd31962560147b2df7e569b437531183f3e0
homepeeps <helloworld@email.com>
2020-06-25 17:21:32 -0400

    added to check for pending snapshot

--- analyzercode/snapshotanalyzer.py
+++ analyzercode/snapshotanalyzer.py
@@ -14,6 +14,11 @@ def filter_instances(project):
     else:
         instances = ec2.instances.all()
     return instances
+
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 @click.group()
 def cli():
     """Snapshot manages snapshots"""
@@ -86,6 +91,9 @@ def create_snapshots(project):
         i.wait_until_stopped()
 
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print(' Skipping {0}, snapshot already in progress'.format(v.id))
+                continue
             print (' Creating snapshot of {0}'.format(v.id))
             v.create_snapshot(Description='Created by our snapshot_analyzer')
 
